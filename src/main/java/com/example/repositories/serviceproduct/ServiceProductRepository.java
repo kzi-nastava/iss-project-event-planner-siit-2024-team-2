c69f4c6b5edf9870b6edb9f547b0d5855e354d71
FilipT03 <filipsombor@gmail.com>
2024-11-26 16:41:22 +0100

    Add event and sp repositories

---
+++ src/main/java/com/example/repositories/serviceproduct/ServiceProductRepository.java
@@ -0,0 +1,8 @@
+package com.example.repositories.serviceproduct;
+
+import com.example.model.event.Event;
+import com.example.model.serviceproduct.ServiceProduct;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServiceProductRepository extends JpaRepository<ServiceProduct, Integer> {
+}
