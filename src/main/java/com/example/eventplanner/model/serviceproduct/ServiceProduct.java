1cd2948d1f560311ccfa9ddbbc077c5ce6b1bc4b
Matija Šeremet <matija.sch03@gmail.com>
2024-11-26 19:13:31 +0100

    Refactor attributes in Service

--- src/main/java/com/example/eventplanner/model/serviceproduct/ServiceProduct.java
+++ src/main/java/com/example/eventplanner/model/serviceproduct/ServiceProduct.java
@@ -1,6 +1,9 @@
 package com.example.eventplanner.model.serviceproduct;
 
+import java.util.List;
+
 import com.example.eventplanner.model.Entity;
+import com.example.eventplanner.model.event.EventType;
 import com.example.eventplanner.model.user.ServiceProductProvider;
 import lombok.*;
 
@@ -9,9 +12,14 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ServiceProduct extends Entity {
+	private ServiceProductCategory category;
     private boolean available;
+    private boolean visible;
     private double price;
+    private double discount;
     private String name;
     private String description;
+    private List<String> images;
+    private List<EventType> availableEventTypes;
     private ServiceProductProvider serviceProductProvider;
 }
