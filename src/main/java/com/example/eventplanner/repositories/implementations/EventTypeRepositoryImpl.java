f758bd8227c4cbe0dec8e927f6ef4b84b6543e0c
cofi420 <filip.vidakovic.04@gmail.com>
2024-11-26 17:02:03 +0100

    Add project structure

---
+++ src/main/java/com/example/eventplanner/repositories/implementations/EventTypeRepositoryImpl.java
@@ -0,0 +1,4 @@
+package com.example.eventplanner.repositories.implementations;
+import com.example.eventplanner.repositories.interfaces.EventTypeRepository;
+public class EventTypeRepositoryImpl implements EventTypeRepository{
+}
---
+++ src/main/java/com/example/eventplanner/repositories/interfaces/EventTypeRepository.java
@@ -0,0 +1,4 @@
+package com.example.eventplanner.repositories.interfaces;
+
+public interface EventTypeRepository {
+}
