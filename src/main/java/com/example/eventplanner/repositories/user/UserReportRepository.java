e7cbd72e4d68e8bfe1cb237b0b81b24dc8ef2bd4
Filip Tot <filipsombor@gmail.com>
2024-11-27 18:49:00 +0100

    Add top5 endpoints for events and serviceproducts

--- src/main/java/com/example/eventplanner/repositories/user/UserReportRepository.java
+++ src/main/java/com/example/eventplanner/repositories/user/UserReportRepository.java
@@ -3,5 +3,5 @@ package com.example.eventplanner.repositories.user;
 import com.example.eventplanner.model.user.UserReport;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserReportRepository extends JpaRepository<UserReport, Integer> {
+public interface UserReportRepository extends JpaRepository<UserReport, Long> {
 }
--- src/main/java/com/example/eventplanner/services/serviceproduct/ServiceProductService.java
+++ src/main/java/com/example/eventplanner/services/serviceproduct/ServiceProductService.java
@@ -1,13 +1,24 @@
 package com.example.eventplanner.services.serviceproduct;
 
+import com.example.eventplanner.model.event.Event;
 import com.example.eventplanner.model.serviceproduct.ServiceProduct;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Service
+@Getter
+@Setter
 public class ServiceProductService {
-    List<ServiceProduct> serviceProducts = new ArrayList<>();
+    Map<Long, ServiceProduct> serviceProducts = new HashMap<>();
     public ServiceProductService() {}
+
+    public List<ServiceProduct> getTop5() {
+        return serviceProducts.values()
+                .stream()
+                .limit(5)
+                .toList();
+    }
 }
