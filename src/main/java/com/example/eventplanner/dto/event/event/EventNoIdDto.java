36a99e6c3363665d88bd607305190a7a2ffa33cc
Filip Tot <filipsombor@gmail.com>
2024-11-27 22:38:51 +0100

    Add Event controller, servis, dto and filtering/pagination

---
+++ src/main/java/com/example/eventplanner/dto/event/event/EventNoIdDto.java
@@ -0,0 +1,29 @@
+package com.example.eventplanner.dto.event.event;
+
+import com.example.eventplanner.model.event.Activity;
+import com.example.eventplanner.model.event.Budget;
+import com.example.eventplanner.model.event.EventType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EventNoIdDto {
+    private String name;
+    private String description;
+    private long typeId;
+    private int maxAttendances;
+    private boolean isOpen;
+    private double longitude;
+    private double latitude;
+    private Date date;
+    private List<Long> activityIds;
+    private List<Long> budgets;
+}
