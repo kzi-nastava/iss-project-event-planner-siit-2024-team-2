a40ea92d9c8a13297b882df171b482fefb6ed087
Filip Tot <filipsombor@gmail.com>
2024-11-27 20:19:41 +0100

    Remove repositories

--- src/main/java/com/example/eventplanner/repositories/serviceproduct/ServiceProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.eventplanner.repositories.serviceproduct;
-
-import com.example.eventplanner.model.serviceproduct.ServiceProduct;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServiceProductRepository extends JpaRepository<ServiceProduct, Long> {
-}
--- src/main/java/com/example/eventplanner/repositories/user/UserReportRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.eventplanner.repositories.user;
-
-import com.example.eventplanner.model.user.UserReport;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserReportRepository extends JpaRepository<UserReport, Long> {
-}
