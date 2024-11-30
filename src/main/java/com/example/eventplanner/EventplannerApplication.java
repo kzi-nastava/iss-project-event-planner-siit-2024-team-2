8b22edc12196a9aaf7cd4dcd0cf36f59ed980e07
cofi420 <filip.vidakovic.04@gmail.com>
2024-11-30 17:27:00 +0100

    Add DB config

--- src/main/java/com/example/eventplanner/EventplannerApplication.java
+++ src/main/java/com/example/eventplanner/EventplannerApplication.java
@@ -2,8 +2,10 @@ package com.example.eventplanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.example.eventplanner.model")
 public class EventplannerApplication {
 
 	public static void main(String[] args) {
