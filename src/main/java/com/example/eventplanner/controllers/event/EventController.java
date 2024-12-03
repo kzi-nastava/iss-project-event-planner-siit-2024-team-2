bc144ee0eedd70cf6144fe21e35d9dc6f528ce08
FilipT03 <filipsombor@gmail.com>
2024-12-03 16:50:03 +0100

    Revert test change

--- src/main/java/com/example/eventplanner/controllers/event/EventController.java
+++ src/main/java/com/example/eventplanner/controllers/event/EventController.java
@@ -22,7 +22,7 @@ import java.util.Date;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/events2")
+@RequestMapping("/api/events")
 @RequiredArgsConstructor()
 public class EventController {
     private final EventService eventService;
