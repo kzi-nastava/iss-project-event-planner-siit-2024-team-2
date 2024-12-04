50ee5c59138200b5630115abfe1acccaeb6886c4
Filip Tot <filipsombor@gmail.com>
2024-12-04 23:38:25 +0100

    Add delete related annotations and withId

--- src/main/java/com/example/eventplanner/model/Entity.java
+++ src/main/java/com/example/eventplanner/model/Entity.java
@@ -5,15 +5,26 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @MappedSuperclass
+@SQLDelete(sql
+        = "UPDATE teacher "
+        + "SET deleted = true "
+        + "WHERE id = ?")
+@SQLRestriction("isActive = true")
 public class Entity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    private boolean isActive;
+    private boolean isActive = true;
+    public Entity withId(long id) {
+        this.id = id;
+        return this;
+    }
 }
