322eecf8bea07b31df4cddcdd07f78f7b7930bd6
FilipT03 <filipsombor@gmail.com>
2025-02-05 23:42:52 +0100

    Add queries for finding creator data

--- src/main/java/com/example/eventplanner/repositories/user/EventOrganizerRepository.java
+++ src/main/java/com/example/eventplanner/repositories/user/EventOrganizerRepository.java
@@ -1,8 +1,20 @@
 package com.example.eventplanner.repositories.user;
 
+import com.example.eventplanner.model.event.EventCreatorProjection;
 import com.example.eventplanner.model.user.EventOrganizer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface EventOrganizerRepository extends JpaRepository<EventOrganizer, Long> {
     boolean existsByEmail(String email);
+
+    @Query(value = "SELECT e.id AS eventId, u.name AS creatorUsername, u.email AS creatorEmail " +
+            "FROM Event e " +
+            "JOIN Users u ON e.creator_id = u.id " +
+            "WHERE e.id IN (:eventIds)",
+            nativeQuery = true)
+    List<EventCreatorProjection> findCreatorsByEventIds(@Param("eventIds") List<Long> eventIds);
 }
--- src/main/java/com/example/eventplanner/repositories/user/ServiceProductProviderRepository.java
+++ src/main/java/com/example/eventplanner/repositories/user/ServiceProductProviderRepository.java
@@ -1,9 +1,23 @@
 package com.example.eventplanner.repositories.user;
 
+import com.example.eventplanner.model.event.EventCreatorProjection;
+import com.example.eventplanner.model.serviceproduct.ServiceProductCreatorProjection;
 import com.example.eventplanner.model.user.ServiceProductProvider;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface ServiceProductProviderRepository extends JpaRepository<ServiceProductProvider, Long> {
     boolean existsByEmail(String email);
+
+    @Query(value = "SELECT sp.id AS serviceProductId, u.name AS creatorUsername, u.email AS creatorEmail " +
+            "FROM ServiceProduct sp " +
+            "JOIN Users u ON sp.creator_id = u.id " +
+            "WHERE sp.id IN (:serviceProductIds)",
+            nativeQuery = true)
+    List<ServiceProductCreatorProjection> findCreatorsByServiceProductIds(
+            @Param("serviceProductIds") List<Long> serviceProductIds);
 }
