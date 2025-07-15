f551263b311c7e44051171f7d3751d590857a0e7
FilipT03 <filipsombor@gmail.com>
2025-07-16 00:58:39 +0200

    Add image request logic and endpoint

---
+++ src/main/java/com/example/eventplanner/services/serviceproduct/ImageService.java
@@ -0,0 +1,54 @@
+package com.example.eventplanner.services.serviceproduct;
+
+import com.example.eventplanner.utils.StatusPair;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Base64;
+
+@Service
+@RequiredArgsConstructor
+public class ImageService {
+    private static final String UPLOADS_DIR = "uploads";
+    public MediaType getMediaType(String path) {
+        String decodedPath = new String(Base64.getDecoder().decode(path));
+        String extension = path.substring(decodedPath.lastIndexOf(".") + 1);
+        return switch (extension) {
+            case "png" -> MediaType.IMAGE_PNG;
+            case "gif" -> MediaType.IMAGE_GIF;
+            default -> MediaType.IMAGE_JPEG;
+        };
+    }
+    public StatusPair getImageStream(String path) {
+        String decodedPath = new String(Base64.getDecoder().decode(path));
+        // TODO: Add authorization
+        Path imagePath = sanitizePath(decodedPath);
+        if (imagePath == null)
+            return new StatusPair(null, HttpStatus.FORBIDDEN);
+        try {
+            return new StatusPair(new FileInputStream(imagePath.toFile()), HttpStatus.OK);
+        }
+        catch (FileNotFoundException e) {
+            return new StatusPair(null, HttpStatus.NOT_FOUND);
+        }
+    }
+    private Path sanitizePath(String decodedPath)  {
+        Path normalizedPath = Paths.get(decodedPath).normalize();
+
+        Path uploadsDir = Paths.get(System.getProperty("user.dir")).resolve(UPLOADS_DIR);
+        Path absolutePath = uploadsDir.resolve(normalizedPath).normalize();
+        System.out.println(absolutePath);
+        System.out.println(uploadsDir);
+
+        if (!absolutePath.startsWith(uploadsDir))
+            return null;
+        return absolutePath;
+    }
+}
