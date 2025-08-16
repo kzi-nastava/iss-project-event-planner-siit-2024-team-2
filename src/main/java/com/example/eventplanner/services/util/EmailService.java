a24aaede555fe8d8ba3bc6fe3c572f9985018d9e
FilipT03 <filipsombor@gmail.com>
2025-08-16 17:49:07 +0200

    Add email service

---
+++ src/main/java/com/example/eventplanner/services/util/EmailService.java
@@ -0,0 +1,23 @@
+package com.example.eventplanner.services.util;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+    private final JavaMailSender emailSender;
+    @Value("${spring.mail.username}") private String sender;
+
+    public void sendSimpleMessage(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("Test");
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        emailSender.send(message);
+    }
+}
