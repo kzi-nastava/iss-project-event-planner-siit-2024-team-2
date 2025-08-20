88931bfe1b6b9041ba27351350b8eec280739663
Matija Šeremet <matija.sch03@gmail.com>
2025-08-20 23:39:11 +0200

    Add pdf generating

--- src/main/java/com/example/eventplanner/controllers/serviceproduct/PriceListController.java
+++ src/main/java/com/example/eventplanner/controllers/serviceproduct/PriceListController.java
@@ -3,6 +3,8 @@ package com.example.eventplanner.controllers.serviceproduct;
 import com.example.eventplanner.dto.serviceproduct.pricelist.PriceListDto;
 import com.example.eventplanner.services.serviceproduct.PriceListService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,5 +32,14 @@ public class PriceListController {
         return updatedPriceListDto != null ?
                 ResponseEntity.ok(updatedPriceListDto) :
                 ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/{id}/pdf")
+    public ResponseEntity<byte[]> getPdf(@PathVariable Long id) throws Exception {
+        byte[] pdf = priceListService.generatePdf (id);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=price_list_" + id + ".pdf")
+                .contentType(MediaType.APPLICATION_PDF)
+                .body(pdf);
     }
 }
--- src/main/java/com/example/eventplanner/services/serviceproduct/PriceListService.java
+++ src/main/java/com/example/eventplanner/services/serviceproduct/PriceListService.java
@@ -4,9 +4,14 @@ import com.example.eventplanner.dto.serviceproduct.pricelist.PriceListDto;
 import com.example.eventplanner.dto.serviceproduct.serviceproduct.ServiceProductMapper;
 import com.example.eventplanner.model.serviceproduct.ServiceProduct;
 import com.example.eventplanner.repositories.serviceproduct.ServiceProductRepository;
+import com.lowagie.text.*;
+import com.lowagie.text.pdf.PdfPCell;
+import com.lowagie.text.pdf.PdfPTable;
+import com.lowagie.text.pdf.PdfWriter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.io.ByteArrayOutputStream;
 import java.util.Collection;
 
 @Service
@@ -27,5 +32,48 @@ public class PriceListService {
                     return ServiceProductMapper.toPriceListItemDto(savedSp);
                 })
                 .orElse(null);
+    }
+
+    public byte[] generatePdf(long id) {
+        Collection<PriceListDto> priceList = getBySppId(id);
+        try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+            Document document = new Document();
+            PdfWriter.getInstance(document, out);
+            document.open();
+
+            Font titleFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD, 18);
+            Paragraph title = new Paragraph("Price List Report", titleFont);
+            title.setAlignment(Element.ALIGN_CENTER);
+            title.setSpacingAfter(20); // space below title
+            document.add(title);
+
+            PdfPTable table = new PdfPTable(5);
+            table.setWidthPercentage(100);
+            table.setSpacingBefore(10f);
+            table.setSpacingAfter(10f);
+
+            Font cellFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD, 12);
+            table.addCell(new PdfPCell(new Phrase("#", cellFont)));
+            table.addCell(new PdfPCell(new Phrase("Name", cellFont)));
+            table.addCell(new PdfPCell(new Phrase("Price", cellFont)));
+            table.addCell(new PdfPCell(new Phrase("Discount", cellFont)));
+            table.addCell(new PdfPCell(new Phrase("Total", cellFont)));
+
+            int i = 1;
+            for (PriceListDto sp : priceList) {
+                table.addCell(String.valueOf(i++));
+                table.addCell(sp.getName());
+                table.addCell(sp.getPrice().toString());
+                table.addCell(sp.getDiscount().toString());
+                table.addCell(sp.getTotal().toString());
+            }
+
+            document.add(table);
+            document.close();
+            return out.toByteArray();
+
+        } catch (Exception e) {
+            throw new RuntimeException("Error generating PDF", e);
+        }
     }
 }
