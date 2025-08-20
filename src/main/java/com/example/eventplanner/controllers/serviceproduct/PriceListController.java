b05b314df53f0c99c9ee022e15f596c3938271e0
Matija Šeremet <matija.sch03@gmail.com>
2025-08-20 21:25:52 +0200

    Add and fix price list layers and dtos

--- src/main/java/com/example/eventplanner/controllers/serviceproduct/PriceListController.java
+++ src/main/java/com/example/eventplanner/controllers/serviceproduct/PriceListController.java
@@ -1,10 +1,8 @@
 package com.example.eventplanner.controllers.serviceproduct;
 
-import com.example.eventplanner.dto.serviceproduct.pricelist.CreatePriceListDto;
 import com.example.eventplanner.dto.serviceproduct.pricelist.PriceListDto;
 import com.example.eventplanner.services.serviceproduct.PriceListService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,27 +14,19 @@ import java.util.Collection;
 public class PriceListController {
     private final PriceListService priceListService;
 
-    @GetMapping()
-    public ResponseEntity<Collection<PriceListDto>> getAllPriceList() {
-        return ResponseEntity.ok(priceListService.getAll());
-    }
-
-    @GetMapping(value = "/{id}")
-    public ResponseEntity<PriceListDto> getPriceListById(@PathVariable("id") Long id) {
-        PriceListDto priceListDto = priceListService.getById(id);
-        return priceListDto != null ?
-                ResponseEntity.ok(priceListDto) :
+    @GetMapping(value = "/{sppId}")
+    public ResponseEntity<Collection<PriceListDto>> getBySppId(@PathVariable("sppId") Long sppId) {
+        Collection<PriceListDto> priceList = priceListService.getBySppId(sppId);
+        return priceList != null ?
+                ResponseEntity.ok(priceList) :
                 ResponseEntity.notFound().build();
     }
 
-    @PostMapping()
-    public ResponseEntity<PriceListDto> createPriceList(@RequestBody CreatePriceListDto createPriceListDto) {
-        return new ResponseEntity<>(priceListService.create(createPriceListDto), HttpStatus.CREATED);
-    }
-
     @PutMapping(value = "/{id}")
-    public ResponseEntity<PriceListDto> updatePriceList(@PathVariable("id") Long id, @RequestBody CreatePriceListDto createPriceListDto) {
-        PriceListDto updatedPriceListDto = priceListService.update(id, createPriceListDto);
+    public ResponseEntity<PriceListDto> updatePriceList(@PathVariable("id") Long id,
+                                                        @RequestParam Double price,
+                                                        @RequestParam Double discount) {
+        PriceListDto updatedPriceListDto = priceListService.update(id, price, discount);
         return updatedPriceListDto != null ?
                 ResponseEntity.ok(updatedPriceListDto) :
                 ResponseEntity.notFound().build();
--- src/main/java/com/example/eventplanner/services/serviceproduct/PriceListService.java
+++ src/main/java/com/example/eventplanner/services/serviceproduct/PriceListService.java
@@ -1,39 +1,31 @@
 package com.example.eventplanner.services.serviceproduct;
 
-import com.example.eventplanner.dto.serviceproduct.pricelist.CreatePriceListDto;
 import com.example.eventplanner.dto.serviceproduct.pricelist.PriceListDto;
+import com.example.eventplanner.dto.serviceproduct.serviceproduct.ServiceProductMapper;
+import com.example.eventplanner.model.serviceproduct.ServiceProduct;
+import com.example.eventplanner.repositories.serviceproduct.ServiceProductRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.concurrent.atomic.AtomicLong;
 
 @Service
+@RequiredArgsConstructor
 public class PriceListService {
-    private final static AtomicLong counter = new AtomicLong();
-    private final HashMap<Long, PriceListDto> priceList = new HashMap<>();
+    private final ServiceProductRepository serviceProductRepository;
 
-    public Collection<PriceListDto> getAll() {
-        return priceList.values();
+    public Collection<PriceListDto> getBySppId(Long sppId) {
+        return serviceProductRepository.getPriceListBySppId(sppId);
     }
 
-    public PriceListDto getById(Long id) {
-        return priceList.get(id);
-    }
-
-    public PriceListDto create(CreatePriceListDto createPriceListDto) {
-        Long id = counter.incrementAndGet();
-        PriceListDto priceListDto = new PriceListDto(id, createPriceListDto);
-        priceList.put(id, priceListDto);
-        return priceListDto;
-    }
-
-    public PriceListDto update(Long id, CreatePriceListDto createPriceListDto) {
-        if (this.getById(id) == null) {
-            return null;
-        }
-        PriceListDto updatedPriceListDto = new PriceListDto(id, createPriceListDto);
-        priceList.put(id, updatedPriceListDto);
-        return updatedPriceListDto;
+    public PriceListDto update(Long id, Double price, Double discount) {
+        return serviceProductRepository.findById(id)
+                .map(sp -> {
+                    sp.setPrice(price);
+                    sp.setDiscount(discount);
+                    ServiceProduct savedSp = serviceProductRepository.save(sp);
+                    return ServiceProductMapper.toPriceListItemDto(savedSp);
+                })
+                .orElse(null);
     }
 }
